dadbed7caba00832172bd7d4bab0771fe61408c2
dimmerz92 <andrew.weymes@outlook.com>
2023-07-22 23:55:17 +0800

    update main function

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#define RAND_RANGE 20
+#include "headers.h"
 
-static pthread_mutex_t w_mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t w_cond = PTHREAD_COND_INITIALIZER;
-static pthread_mutex_t r_mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t r_cond = PTHREAD_COND_INITIALIZER;
-static int readers = 0;
-static int sum = 0;
+struct shared_data shared = {
+    .decrementer = NULL,
+    .incrementer = NULL,
+    .writers = 0,
+    .sum = 0
+};
 
-
-
-void reader(void);
-void writer(void);
+pthread_mutex_t w_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t w_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t r_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t r_cond = PTHREAD_COND_INITIALIZER;
+int reader_count = 0;
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
@@ -23,16 +20,41 @@ int main(int argc, char *argv[]) {
     int n_readers = rand() % RAND_RANGE;
 
     //create incrementer threads
+    pthread_t incrementers[n_incrementers];
     for (int i = 0; i < n_incrementers; i++) {
-        // throw threads
+        writer_metadata md = {
+            .id = i,
+            .incdec = 1
+        };
+        pthread_create(&incrementers[i], NULL, writer, (void *)&md);
     }
     //create decrementer threads
+    pthread_t decrementers[n_decrementers];
     for (int i = 0; i < n_decrementers; i++) {
-        //throw threads
+        writer_metadata md = {
+            .id = i,
+            .incdec = -1
+        };
+        pthread_create(&decrementers[i], NULL, writer, (void *)&md);
     }
     //create reader threads
+    pthread_t readers[n_readers];
     for (int i = 0; i < n_readers; i++) {
-        //throw threads
+        pthread_create(&readers[i], NULL, reader, (void *)&i);
     }
-    return 0;
+
+    //join threads
+    join_threads(incrementers, n_incrementers);
+    join_threads(decrementers, n_decrementers);
+    join_threads(readers, n_readers);
+
+    printf("There were %d readers, %d incrementers, and %d decrementers\n",
+            n_readers, n_incrementers, n_decrementers);
+    printf("The final state of the data is:\n");
+    printf("\tlast incrementer: %d\n", shared.incrementer);
+    printf("\tlast decrementer: %d\n", shared.decrementer);
+    printf("\ttotal writers: %d\n", shared.writers);
+    printf("sum %D\n", shared.sum);
+    
+    return 0;   
 }
