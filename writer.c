8632b7b276df5228fb3aeee2ee5398e926cb763a
dimmerz92 <andrew.weymes@outlook.com>
2023-07-22 23:10:27 +0800

    update writed function to take single struct

--- writer.c
+++ writer.c
@@ -1,16 +1,16 @@
 #include "headers.h"
 
-void writer(int id, int incdec) {
+void writer(writer_metadata md) {
     //lock w mutex
     pthread_mutex_lock(&w_mutex);
     //increment or decrement
-    shared.sum += incdec;
-    if (incdec > 0) {
-        shared.incrementer = id;
-        printf("Incrementer %d set sum = %d\n", id, shared.sum);
+    shared.sum += md.incdec;
+    if (md.incdec > 0) {
+        shared.incrementer = md.id;
+        printf("Incrementer %d set sum = %d\n", md.id, shared.sum);
     } else {
-        shared.decrementer = id;
-        printf("Decrementer %d set sum = %d\n", id, shared.sum);
+        shared.decrementer = md.id;
+        printf("Decrementer %d set sum = %d\n", md.id, shared.sum);
     }
     //unlock w mutex
     pthread_mutex_unlock(&w_mutex);
